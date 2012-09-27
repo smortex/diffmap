41f2ecbe9bf53d3eb4c6f4e0eb0f5be549f53414
Romain Tartière <romain@blogreen.org>
2012-09-27 12:42:22 +0200

    Refactor code and return the number of different blocks.

--- diffmap.c
+++ diffmap.c
@@ -13,6 +13,59 @@ usage (void)
 }
 
 int
+compare_files (char *filename1, char *filename2, int block_size, int screen_width)
+{
+    int result = 0;
+    int block_count = 0;
+    int identical_block_count = 0;
+
+    FILE *file1, *file2;
+
+    if (!(file1 = fopen (filename1, "r")))
+	err (EXIT_FAILURE, "%s", filename1);
+    if (!(file2 = fopen (filename2, "r")))
+	err (EXIT_FAILURE, "%s", filename2);
+
+    char *buf1, *buf2;
+    if (!(buf1 = malloc (block_size)))
+	err (EXIT_FAILURE, "Cannot allocate memory");
+    if (!(buf2 = malloc (block_size)))
+	err (EXIT_FAILURE, "Cannot allocate memory");
+
+    int line_position = 0;
+    while (!feof (file1) || !feof (file2)) {
+	int read1, read2;
+
+	block_count++;
+
+	read1 = fread (buf1, 1, block_size, file1);
+	read2 = fread (buf2, 1, block_size, file2);
+
+	if ((read1 != read2) && (read1 == 0))
+	    line_position += fprintf (stdout, "/");
+	else if ((read1 != read2) && (read2 == 0))
+	    line_position += fprintf (stdout, "\\");
+	else if (memcmp (buf1, buf2, read1))
+	    line_position += fprintf (stdout, "X");
+	else {
+	    identical_block_count++;
+	    line_position += fprintf (stdout, ".");
+	}
+
+	if (line_position == screen_width) {
+	    fprintf (stdout, "\n");
+	    line_position = 0;
+	}
+    }
+    fprintf (stdout, "\n");
+
+    fclose (file1);
+    fclose (file2);
+
+    return block_count - identical_block_count;
+}
+
+int
 main (int argc, char *argv[])
 {
     int block_size = 512;
@@ -39,44 +92,8 @@ main (int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
-    FILE *file1, *file2;
+    int ret;
+    ret = compare_files (argv[0], argv[1], block_size, screen_width);
 
-    if (!(file1 = fopen (argv[0], "r")))
-	err (EXIT_FAILURE, "%s", argv[0]);
-    if (!(file2 = fopen (argv[1], "r")))
-	err (EXIT_FAILURE, "%s", argv[1]);
-
-    char *buf1, *buf2;
-    if (!(buf1 = malloc (block_size)))
-	err (EXIT_FAILURE, "Cannot allocate memory");
-    if (!(buf2 = malloc (block_size)))
-	err (EXIT_FAILURE, "Cannot allocate memory");
-
-    int line_position = 0;
-    while (!feof (file1) || !feof (file2)) {
-	int read1, read2;
-
-	read1 = fread (buf1, 1, block_size, file1);
-	read2 = fread (buf2, 1, block_size, file2);
-
-	if ((read1 != read2) && (read1 == 0))
-	    line_position += fprintf (stdout, "/");
-	else if ((read1 != read2) && (read2 == 0))
-	    line_position += fprintf (stdout, "\\");
-	else if (memcmp (buf1, buf2, read1))
-	    line_position += fprintf (stdout, "X");
-	else
-	    line_position += fprintf (stdout, ".");
-
-	if (line_position == screen_width) {
-	    fprintf (stdout, "\n");
-	    line_position = 0;
-	}
-    }
-    fprintf (stdout, "\n");
-
-    fclose (file1);
-    fclose (file2);
-
-    exit(EXIT_SUCCESS);
+    exit (ret);
 }
